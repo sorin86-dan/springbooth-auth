9aa77f023c0efa2085dcf47f5f3e82480481729c
Sorin Dan <dansorin2006@gmail.com>
2022-09-28 21:47:29 +0300

    feat(master): Fixed Spring Cloud Contract test

--- src/test/java/com/testing/scc/SpringCloudContractTest.java
+++ src/test/java/com/testing/scc/SpringCloudContractTest.java
@@ -1,20 +1,27 @@
 package com.testing.scc;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.net.URI;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,47 +33,44 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         stubsMode = StubRunnerProperties.StubsMode.LOCAL,
         ids = "com.testing:springboot-db:+:stubs:8090")
 public class SpringCloudContractTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
     @Test
     public void testSetDbMessage() throws Exception {
-        Map<String, String> headers = new HashMap<>();
+        URI uri = new URI("http://localhost:8090/set-db-message");
+        HttpEntity<String> request = generateRequest("OK");
+        ResponseEntity<String> result = new TestRestTemplate().postForEntity(uri, request, String.class);
 
-        headers.put("Content-Type", "application/json");
-        headers.put("id", "OK");
-
-        mockMvc.perform(MockMvcRequestBuilders.post("/set-db-message")
-                        .header("Content-Type", "application/json")
-                        .header("id", "OK")
-                        .content("{\"message\":\"Baza de date aleasa este: \"}"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("Message updated successfully!"));
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        assertEquals("Message updated successfully!", result.getBody());
     }
+
     @Test
     public void testSetDbMessageIdEmpty() throws Exception {
-        Map<String, String> headers = new HashMap<>();
+        URI uri = new URI("http://localhost:8090/set-db-message");
+        HttpEntity<String> request = generateRequest("");
+        ResponseEntity<String> result = new TestRestTemplate().postForEntity(uri, request, String.class);
 
-        headers.put("Content-Type", "application/json");
-        headers.put("id", "");
-
-        mockMvc.perform(MockMvcRequestBuilders.post("/set-db-message")
-                        .header("Content-Type", "application/json")
-                        .header("id", "OK")
-                        .content("{\"message\":\"Baza de date aleasa este: \"}"))
-                .andExpect(status().is4xxClientError())
-                .andExpect(content().string("Authorization failed"));
+        assertEquals(HttpStatus.UNAUTHORIZED, result.getStatusCode());
+        assertEquals("Authorization failed", result.getBody());
     }
 
     @Test
     public void testSetDbMessageIdInvalid() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/set-db-message")
-                        .header("Content-Type", "application/json")
-                        .header("id", "OK")
-                        .content("{\"message\":\"Baza de date aleasa este: \"}"))
-                .andExpect(status().is4xxClientError())
-                .andExpect(content().string("Message updated successfully!"));
+        URI uri = new URI("http://localhost:8090/set-db-message");
+        HttpEntity<String> request = generateRequest("DUMMY");
+        ResponseEntity<String> result = new TestRestTemplate().postForEntity(uri, request, String.class);
+
+        assertEquals(HttpStatus.UNAUTHORIZED, result.getStatusCode());
+        assertEquals("Authorization failed", result.getBody());
     }
 
+
+
+    @NotNull
+    private HttpEntity<String> generateRequest(String ok) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+        headers.set("id", ok);
+        String jsonBody = "{\"message\":\"Baza de date aleasa: \"}";
+        return new HttpEntity<>(jsonBody, headers);
+    }
 }
